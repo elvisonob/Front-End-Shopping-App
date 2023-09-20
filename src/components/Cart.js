876748ff728035f2287dad44aa886ec3b95198a0
elvisonob <elvisonob@yahoo.com>
2023-09-20 04:17:41 +0100

    adjusting functionalities

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,12 +3,19 @@ import classes from './Cart.module.css';
 
 const Cart = (props) => {
   const [listOfItems, setListOfItems] = useState([]);
+
+  const displayArray = () => {
+    setListOfItems(props.onClick);
+  };
+
   return (
     <div className={classes['cart-container']}>
       <h2>Cart</h2>
       <p>Added Items Display</p>
       <div className={classes['sub-container-column']}>
-        <p>{props.name}</p>
+        {listOfItems.map((items) => (
+          <p>{items.props.name}</p>
+        ))}
         <p>Quantity</p>
         <button>+</button>
         <button>-</button>
