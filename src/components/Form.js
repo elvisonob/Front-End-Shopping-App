ef8810bb9035a98ef915521324bbe4e75ff2b7e0
elvisonob <elvisonob@yahoo.com>
2023-10-02 04:58:50 +0100

    adding to cart manually

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Form.css';
+
 const Form = () => {
+  const [productName, setProductName] = useState('');
+  const [productAmount, setProductAmount] = useState('');
+  const [errorName, setErrorName] = useState(null);
+  const [errorAmount, setErrorAmount] = useState(null);
+
+  const onProductName = (event) => {
+    setProductName(event.target.value);
+  };
+
+  const onProductAmount = (event) => {
+    setProductAmount(event.target.value);
+  };
+
+  const onSubmitForm = () => {
+    console.log(productName, productAmount);
+  };
+
   return (
-    <div>
+    <div className="form">
       <form>
-        <label htmlFor="name">Product Name</label>
-        <input type="text" id="name" />
-        <label htmlFor="number">Use by Date</label>
-        <input type="date" />
-        <label htmlFor="number">Amount</label>
-        <input type="number" id="number" />
+        <label htmlFor="name">Product Name </label>
+        <input type="text" id="name" onChange={onProductName} />
+        <br />
+        {errorName}
+        <br />
+        <br />
+        {/* <label htmlFor="number">Use by Date</label>
+        <input type="date" /> */}
+        <label htmlFor="number">Amount </label>
+        <input type="number" id="number" onChange={onProductAmount} />
+        <br />
+        {errorAmount}
       </form>
-      <button>Add to Cart</button>
+      <br />
+      <button onClick={onSubmitForm}>Add to Cart </button>
     </div>
   );
 };
